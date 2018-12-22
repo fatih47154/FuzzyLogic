db22e06a66bb397d905e513f0977775476f377c9
Fatih <fatih47154@gmail.com>
2018-12-22 19:34:09 +0300

    Chart

--- FuzzyLogic/Controllers/IndexController.cs
+++ FuzzyLogic/Controllers/IndexController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FuzzyLogic.Models;
+using Newtonsoft.Json;
 
 namespace FuzzyLogic.Controllers
 {
@@ -11,6 +13,37 @@ namespace FuzzyLogic.Controllers
         // GET: Index
         public ActionResult Index()
         {
+            List<DataPoint> dataPoints1 = new List<DataPoint>();
+            List<DataPoint> dataPoints2 = new List<DataPoint>();
+            List<DataPoint> dataPoints3 = new List<DataPoint>();
+            List<DataPoint> dataPoints4 = new List<DataPoint>();
+            List<DataPoint> dataPoints5 = new List<DataPoint>();
+
+            dataPoints1.Add(new DataPoint(0, 1));
+            dataPoints1.Add(new DataPoint(10, 1));
+            dataPoints1.Add(new DataPoint(20, 0));
+
+            dataPoints2.Add(new DataPoint(15, 0));
+            dataPoints2.Add(new DataPoint(27.5, 1));
+            dataPoints2.Add(new DataPoint(40, 0));
+
+            dataPoints3.Add(new DataPoint(35, 0));
+            dataPoints3.Add(new DataPoint(47.5, 1));
+            dataPoints3.Add(new DataPoint(60, 0));
+
+            dataPoints4.Add(new DataPoint(55, 0));
+            dataPoints4.Add(new DataPoint(67.5, 1));
+            dataPoints4.Add(new DataPoint(80, 0));
+
+            dataPoints5.Add(new DataPoint(75, 0));
+            dataPoints5.Add(new DataPoint(87.5, 1));
+            dataPoints5.Add(new DataPoint(100, 0));
+
+            ViewBag.DataPoints1 = JsonConvert.SerializeObject(dataPoints1);
+            ViewBag.DataPoints2 = JsonConvert.SerializeObject(dataPoints2);
+            ViewBag.DataPoints3 = JsonConvert.SerializeObject(dataPoints3);
+            ViewBag.DataPoints4 = JsonConvert.SerializeObject(dataPoints3);
+            ViewBag.DataPoints4 = JsonConvert.SerializeObject(dataPoints4);
 
             return View();
         }
